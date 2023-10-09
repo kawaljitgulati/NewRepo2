84d63236c9a5fa2c477ec55f5c2b120776ac4b2a
kawaljitgulati <46310041+kawaljitgulati@users.noreply.github.com>
2023-10-09 21:16:15 +0100

    Updated TFL assignment

---
+++ TFL New/JourneyPlanner.cs
@@ -0,0 +1,152 @@
+﻿using TechTalk.SpecFlow;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+
+namespace TFL_New
+{
+    [Binding]
+
+    public class JourneyPlanner
+
+    {
+        IWebDriver driver;
+        
+
+
+        [Given(@"I am on the TFL journey planning page ""([^""]*)""")]
+        public void GivenIAmOnTheTFLJourneyPlanningPage(string p0)
+        {
+            driver = new ChromeDriver();
+            driver.Navigate().GoToUrl(p0);
+            driver.FindElement (By.Id("CybotCookiebotDialogBodyLevelButtonLevelOptinAllowAll")).Click();    // Accept Cookie on TFL main Page
+        }
+
+        [When(@"I enter a valid start location ""([^""]*)""")]
+        public void WhenIEnterAValidStartLocation(string bank)
+        {
+            driver.FindElement(By.Id("InputFrom")).SendKeys("Bank");   
+        }
+
+        [When(@"I enter the valid destination location ""([^""]*)""")]
+        public void WhenIEnterTheValidDestinationLocation(string p0)
+        {
+            driver.FindElement(By.Id("InputTo")).SendKeys("Ealing Broadway");
+        }
+
+   
+         [When(@"I click the Enter")]
+        public void WhenIClickTheEnter()
+        {
+            driver.FindElement(By.Id("InputTo")).Submit();
+
+        }
+        [When(@"I click the button ""([^""]*)""")]
+        public void WhenIClickTheButton(string p0)
+        {
+             driver.FindElement(By.Id("plan-journey-button")).Click();
+        }
+
+        [Then(@"I see Journey results")]
+        public void ThenISeeJourneyResults()
+        {
+            Assert.AreEqual(true,driver.PageSource.Contains("Journey results"));
+        }
+
+        [When(@"I enter the wrong start location ""([^""]*)""")]
+        public void WhenIEnterTheWrongStartLocation(string xdf)
+        {
+            driver.FindElement(By.Id("InputFrom")).SendKeys("xdf");
+        }
+
+        [When(@"I enter the wrong destination ""([^""]*)""")]
+        public void WhenIEnterTheWrongDestination(string dft)
+        {
+            driver.FindElement(By.Id("InputTo")).SendKeys("dft");
+        }
+
+        [Then(@"I see ""([^""]*)""")]
+        public void ThenISee(string p0)
+        {
+            Assert.AreEqual(true, driver.PageSource.Contains("Sorry, we can't find a journey matching your criteria"));
+
+        }
+
+        [When(@"I entered no information in start location")]
+        public void WhenIEnteredNoInformationInStartLocation()
+        {
+            driver.FindElement(By.Id("InputFrom")).SendKeys("");
+        }
+
+        [When(@"I entered no information in the destination")]
+        public void WhenIEnteredNoInformationInTheDestination()
+        {
+            driver.FindElement(By.Id("InputTo")).SendKeys("");
+        }
+                   
+        [Then(@"I see error to fill the required field")]
+        public void ThenISeeErrorToFillTheRequiredField()
+        {
+            Assert.AreEqual(true, driver.PageSource.Contains("The From field is required."));
+            Assert.AreEqual(true, driver.PageSource.Contains("The To field is required"));
+        }
+
+
+
+
+
+       [When(@"I see Journey results")]
+       public void WhenISeeJourneyResults()
+       {
+            Assert.AreEqual(true, driver.PageSource.Contains("Journey results"));
+        }
+
+        [When(@"I can click ""([^""]*)"" to change journey details")]
+        public void WhenICanClickToChangeJourneyDetails(string p0)
+        {
+            driver.FindElement(By.XPath("//span[contains(.,'Edit journey')]")).Click();
+           
+        }
+
+        [When(@"I click the arrow to reverse the Journey details")]
+        public void WhenIClickTheArrowToReverseTheJourneyDetailsUnableToLocateTheIdForAButton()
+        {
+            driver.FindElement(By.XPath("//*[@id='plan-a-journey']//a[contains(.,'Switch from and to')]")).Click();
+
+        }
+
+        [When(@"I click Plan a journey")]
+        public void WhenIClickPlanAJourney()
+        {
+            driver.FindElement(By.XPath("//a[contains(.,'Plan a journey')]")).Click();
+        }
+
+        [When(@"I click ""([^""]*)"" text")]
+        public void WhenIClickText(string recents)
+        {
+            driver.FindElement(By.XPath("//a[contains(.,'Recents')]")).Click();
+        }
+
+        [When(@"I enter a valid new start location ""([^""]*)""")]
+        public void WhenIEnterAValidNewStartLocation(string p0)
+        {
+            driver.FindElement(By.Id("InputFrom")).SendKeys(p0);
+        }
+
+        [When(@"I enter the valid new destination location ""([^""]*)""")]
+        public void WhenIEnterTheValidNewDestinationLocation(string p0)
+        {
+            driver.FindElement(By.Id("InputTo")).SendKeys(p0);
+        }
+
+
+        [Then(@"I can see the recent Jounrey result for ""([^""]*)""")]
+        public void ThenICanSeeTheRecentJounreyResultFor(string p0)
+        {
+            Assert.AreEqual(true, driver.PageSource.Contains(p0));
+        }
+
+
+    }
+}
---
+++ TFL New/JourneyPlanner.feature
@@ -0,0 +1,50 @@
+﻿Feature: JourneyPlanner
+
+#1.	Verify that a valid journey can be planned using the widget. (A valid journey will consist of a valid locations entered into the widget)
+Scenario: Validate the Journey 
+	Given I am on the TFL journey planning page "https://tfl.gov.uk/plan-a-journey/"
+	When I enter a valid start location "Bank"       
+	And I enter the valid destination location "Ealing Broadway"  
+	And I click the Enter 
+	Then I see Journey results
+
+
+
+Scenario: Unable to Provide Result when In-Valid Journey 
+	Given  I am on the TFL journey planning page "https://tfl.gov.uk/plan-a-journey/"
+	When I enter the wrong start location "xdf"
+	And I enter the wrong destination "dft"
+	And I click the button "plan-journey-button"
+	Then I see "Sorry, we can't find a journey matching your criteria"
+
+
+Scenario: Unable to Plan when no information entered
+	Given I am on the TFL journey planning page "https://tfl.gov.uk/plan-a-journey/"
+	When I entered no information in start location 
+	And I entered no information in the destination 
+	And I click the Enter
+	Then I see error to fill the required field
+
+	Scenario: Edit Journey
+	Given I am on the TFL journey planning page "https://tfl.gov.uk/plan-a-journey/"
+	When I enter a valid start location "Bank"
+	And I enter the valid destination location "Ealing Broadway"   
+	And I click the Enter
+	And I see Journey results
+	And I can click "Edit Journey" to change journey details 
+	And I click the arrow to reverse the Journey details 
+	And I click the button "plan-journey-button"
+	Then I see Journey results
+
+
+	Scenario: Recent Tab 
+	Given I am on the TFL journey planning page "https://tfl.gov.uk/plan-a-journey/"
+	When I enter a valid new start location "27 Cornell Way, Romford"       
+	And I enter the valid new destination location "45 Cornell Way, Romford"  
+	And I click the Enter 
+	And I see Journey results
+	And I click Plan a journey
+	And I click "Recents" text 
+	Then I can see the recent Jounrey result for "27 Cornell Way to 45 Cornell Way"
+
+
